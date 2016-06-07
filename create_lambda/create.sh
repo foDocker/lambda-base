5748b05a37c375063f34eda062cc8ddb5bf0afba
FCO <fernandocorrea@gmail.com>
2016-06-07 00:49:05 -0300

    create

--- create_lambda/create.sh
+++ create_lambda/create.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-IMAGE=blablablaimg
-LAMBDA_ID=1234567890
+SRC=$1
+LAMBDA_ID=lambda_$(cat $(find . -type f) | md5)
 
+if [ ! -z $LAMBDA_ID ]
+then
+	DIRPATH=/tmp/$LAMBDA_ID
+	rm -rf $DIRPATH
+	cp -r $SRC $DIRPATH
 
+	docker build -t $LAMBDA_ID $DIRPATH
+	echo $IMAGE
+	DATA='{"image": "'$LAMBDA_ID'", "lambda_id": "'$LAMBDA_ID'"}'
+	echo $DATA | node_modules/mustache/bin/mustache - fodocker.yml.mustache > $DIRPATH/fodocker.yml
 
-echo "{\"image\": \"$IMAGE\", \"lambda_id\": \"$LAMBDA_ID\"}" | node_modules/mustache/bin/mustache - fodocker.yml.mustache
+	# SEND TO FODOCKER
+fi
