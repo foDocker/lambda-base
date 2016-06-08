dc9e59bd14d079cf560e0cbb88cc074259818e60
FCO <fernandocorrea@gmail.com>
2016-06-08 01:08:30 -0300

    WiP

--- create_lambda/create.sh
+++ create_lambda/create.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SRC=$1
-LAMBDA_ID=lambda_$(cat $(find . -type f) | md5)
+FODOCKER_NET=lambdabase_default
+#FODOCKER_NET=fodocker_default
+LAMBDA_ID=lambda_$(cat $(find $SRC -type f) | md5)
 
 if [ ! -z $LAMBDA_ID ]
 then
@@ -15,4 +17,5 @@ then
 	echo $DATA | node_modules/mustache/bin/mustache - fodocker.yml.mustache > $DIRPATH/fodocker.yml
 
 	# SEND TO FODOCKER
+	docker run -v $DIRPATH/fodocker.yml:/tmp/fodocker.yml:ro --net=$FODOCKER_NET byrnedo/alpine-curl -F "file=@/tmp/fodocker.yml" http://watcher:3000/
 fi
